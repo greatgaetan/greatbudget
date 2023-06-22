e37755036dbb1577811f45152cc3a40fe371e04d
greatgaetan <gaetan@keiz.ai>
2023-06-22 16:32:14 +0200

    removed robots

--- app/layout.tsx
+++ app/layout.tsx
@@ -53,15 +53,6 @@ export const metadata = {
   },
   manifest: `${siteConfig.url}/site.webmanifest`,
   description: siteConfig.description,
-  robots: {
-    index: true,
-    follow: true,
-    nocache: true,
-    googleBot: {
-      index: true,
-      follow: true,
-    },
-  },
 }
 
 const inter = Inter({
