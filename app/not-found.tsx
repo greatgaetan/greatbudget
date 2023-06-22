339774973f4eb933de30f270da5f77e2386c3256
greatgaetan <gaetan@keiz.ai>
2023-06-22 16:15:52 +0200

    repush

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,7 +6,7 @@ import React from "react"
 
 export const metadata = {
   title: "Not Found",
-  description: "Page not found",
+  description: "The page you are looking for does not exist",
 }
 
 export default function NotFound() {
